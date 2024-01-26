9b2582f42a66bd04056f657e9df8db8187f6f67e
Waleed Ahmed <156175348+LPGangster@users.noreply.github.com>
2024-01-26 22:44:38 +0500

    Delete Lab_02/Test.cpp

--- Lab_02/Test.cpp
+++
@@ -1,7 +0,0 @@
-#include <iostream>
-using namespace std;
-int main()
-{
-     cout << "Testing G++ Compiler!!!!!!!" << endl;
-     cout << "Yes, G++ compiler is working perfectly!!!!!!!" << endl;
-}
