7237f0fd27a29d2b150c05be8ac6b1cd9a44dbe3
Waleed Ahmed <156175348+LPGangster@users.noreply.github.com>
2024-01-26 14:28:04 +0500

    Updated on 26\01\2024

---
+++ Lab_02/Test.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+     cout << "Testing G++ Compiler!!!!!!!" << endl;
+     cout << "Yes, G++ compiler is working perfectly!!!!!!!" << endl;
+}
