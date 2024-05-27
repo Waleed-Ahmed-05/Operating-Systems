bb19ed91542901ac312931421d1b38abb5bcb4dc
Waleed Ahmed <156175348+Waleed-Ahmed-05@users.noreply.github.com>
2024-05-27 23:11:52 +0500

    Update cf-field.h

--- Virtual-OS-Project/Game Box/src/connect-four/cf-field.h
+++ Virtual-OS-Project/Game Box/src/connect-four/cf-field.h
@@ -36,16 +36,3 @@ CfField         *cf_field_new_at        (gint col,
                                          gint row);
 
 #endif /* __CF_FIELD_H__ */
-
-/*
- * Editor modelines  -  https://www.wireshark.org/tools/modelines.html
- *
- * Local variables:
- * c-basic-offset: 8
- * tab-width: 8
- * indent-tabs-mode: nil
- * End:
- *
- * vi: set shiftwidth=8 tabstop=8 expandtab:
- * :indentSize=8:tabSize=8:noTabs=true:
- */
