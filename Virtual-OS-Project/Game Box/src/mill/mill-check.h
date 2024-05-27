9462323aa93d30a38c317e1a5901275754ec30d9
Waleed Ahmed <156175348+Waleed-Ahmed-05@users.noreply.github.com>
2024-05-27 23:05:06 +0500

    Update mill-check.h

--- Virtual-OS-Project/Game Box/src/mill/mill-check.h
+++ Virtual-OS-Project/Game Box/src/mill/mill-check.h
@@ -16,16 +16,3 @@ gint            mill_check_get_field_in_mill    (MillField *field[7][7],
                                                  MillField *piece);
 
 #endif /* __MILL_CHECK_H__ */
-
-/*
- * Editor modelines  -  https://www.wireshark.org/tools/modelines.html
- *
- * Local variables:
- * c-basic-offset: 8
- * tab-width: 8
- * indent-tabs-mode: nil
- * End:
- *
- * vi: set shiftwidth=8 tabstop=8 expandtab:
- * :indentSize=8:tabSize=8:noTabs=true:
- */
