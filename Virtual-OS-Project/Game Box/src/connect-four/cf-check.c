5def434459a940516b7c3e52daac05c85085707b
Waleed Ahmed <156175348+Waleed-Ahmed-05@users.noreply.github.com>
2024-05-27 23:11:10 +0500

    Update cf-check.c

--- Virtual-OS-Project/Game Box/src/connect-four/cf-check.c
+++ Virtual-OS-Project/Game Box/src/connect-four/cf-check.c
@@ -102,16 +102,3 @@ CfField *cf_check_get_lowest_field(CfField *field[7][6],
 
         return NULL;
 }
-
-/*
- * Editor modelines  -  https://www.wireshark.org/tools/modelines.html
- *
- * Local variables:
- * c-basic-offset: 8
- * tab-width: 8
- * indent-tabs-mode: nil
- * End:
- *
- * vi: set shiftwidth=8 tabstop=8 expandtab:
- * :indentSize=8:tabSize=8:noTabs=true:
- */
