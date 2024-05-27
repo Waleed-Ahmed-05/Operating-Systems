8879b22c7e4f4a8badd5ede84367891a29b04d22
Waleed Ahmed <156175348+Waleed-Ahmed-05@users.noreply.github.com>
2024-05-27 23:10:40 +0500

    Update chks-view.c

--- Virtual-OS-Project/Game Box/src/checkers/chks-view.c
+++ Virtual-OS-Project/Game Box/src/checkers/chks-view.c
@@ -320,16 +320,3 @@ ChksView *chks_view_new(void)
 
         return view;
 }
-
-/*
- * Editor modelines  -  https://www.wireshark.org/tools/modelines.html
- *
- * Local variables:
- * c-basic-offset: 8
- * tab-width: 8
- * indent-tabs-mode: nil
- * End:
- *
- * vi: set shiftwidth=8 tabstop=8 expandtab:
- * :indentSize=8:tabSize=8:noTabs=true:
- */
