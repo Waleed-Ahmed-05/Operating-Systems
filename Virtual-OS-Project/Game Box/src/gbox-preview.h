2dbb6c62b16f3dafcf5b72ec2a7333cd92114940
Waleed Ahmed <156175348+Waleed-Ahmed-05@users.noreply.github.com>
2024-05-27 23:07:20 +0500

    Update gbox-preview.h

--- Virtual-OS-Project/Game Box/src/gbox-preview.h
+++ Virtual-OS-Project/Game Box/src/gbox-preview.h
@@ -18,16 +18,3 @@ GtkWidget       *gbox_preview_get_start_button          (GBoxPreview *preview);
 GBoxPreview     *gbox_preview_new                       (void);
 
 #endif /* __GBOX_PREVIEW_H__ */
-
-/*
- * Editor modelines  -  https://www.wireshark.org/tools/modelines.html
- *
- * Local variables:
- * c-basic-offset: 8
- * tab-width: 8
- * indent-tabs-mode: nil
- * End:
- *
- * vi: set shiftwidth=8 tabstop=8 expandtab:
- * :indentSize=8:tabSize=8:noTabs=true:
- */
