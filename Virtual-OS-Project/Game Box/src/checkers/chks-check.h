9ae86bd02dfb1207e9643dad394d59ff94179996
Waleed Ahmed <156175348+Waleed-Ahmed-05@users.noreply.github.com>
2024-05-27 23:09:49 +0500

    Update chks-check.h

--- Virtual-OS-Project/Game Box/src/checkers/chks-check.h
+++ Virtual-OS-Project/Game Box/src/checkers/chks-check.h
@@ -21,16 +21,3 @@ gboolean        chks_check_get_can_jump_again   (ChksField *field[8][8],
 gboolean        chks_check_get_becomes_queen    (ChksField *piece);
 
 #endif /* __CHKS_CHECK_H__ */
-
-/*
- * Editor modelines  -  https://www.wireshark.org/tools/modelines.html
- *
- * Local variables:
- * c-basic-offset: 8
- * tab-width: 8
- * indent-tabs-mode: nil
- * End:
- *
- * vi: set shiftwidth=8 tabstop=8 expandtab:
- * :indentSize=8:tabSize=8:noTabs=true:
- */
