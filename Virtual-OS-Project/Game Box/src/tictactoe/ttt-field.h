5ba9838dee82ae9865667d44c012618cf588ac2e
Waleed Ahmed <156175348+Waleed-Ahmed-05@users.noreply.github.com>
2024-05-27 23:15:36 +0500

    Update ttt-field.h

--- Virtual-OS-Project/Game Box/src/tictactoe/ttt-field.h
+++ Virtual-OS-Project/Game Box/src/tictactoe/ttt-field.h
@@ -31,16 +31,3 @@ void             ttt_field_set_preowner (TttField *field,
 TttField        *ttt_field_new          (void);
 
 #endif /* __TTT_FIELD_H__ */
-
-/*
- * Editor modelines  -  https://www.wireshark.org/tools/modelines.html
- *
- * Local variables:
- * c-basic-offset: 8
- * tab-width: 8
- * indent-tabs-mode: nil
- * End:
- *
- * vi: set shiftwidth=8 tabstop=8 expandtab:
- * :indentSize=8:tabSize=8:noTabs=true:
- */
