2efd6ac6d5896a5ba14fbdd08c597c8c4c295f71
Waleed Ahmed <156175348+Waleed-Ahmed-05@users.noreply.github.com>
2024-05-27 23:10:11 +0500

    Update chks-field.c

--- Virtual-OS-Project/Game Box/src/checkers/chks-field.c
+++ Virtual-OS-Project/Game Box/src/checkers/chks-field.c
@@ -193,16 +193,3 @@ ChksField *chks_field_new_at(gint col,
 
         return field;
 }
-
-/*
- * Editor modelines  -  https://www.wireshark.org/tools/modelines.html
- *
- * Local variables:
- * c-basic-offset: 8
- * tab-width: 8
- * indent-tabs-mode: nil
- * End:
- *
- * vi: set shiftwidth=8 tabstop=8 expandtab:
- * :indentSize=8:tabSize=8:noTabs=true:
- */
